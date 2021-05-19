a5545bd24c21c36d01fb3d778d3a6c71a088d33a
haxqer <2845513252@qq.com>
2021-05-19 15:39:12 +0800

    init

---
+++ tcb/tcb.go
@@ -0,0 +1,16 @@
+package tcb
+
+import "github.com/haxqer/wechat/context"
+
+//Tcb Tencent Cloud Base
+type Tcb struct{
+	*context.Context
+}
+
+//NewTcb new Tencent Cloud Base
+func NewTcb(context *context.Context)*Tcb{
+	return &Tcb{
+		context,
+	}
+}
+
